c79bfde91ebb62572e4fdc5e476be46fcc4ed0b5
ewrefads <clara.donovan@outlook.com>
2025-10-20 14:49:12 +0200

    Added remaining interfaces

---
+++ Warehouse Managemet System/Parser/IParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse_Managemet_System.Parser
+{
+    public interface IParser
+    {
+        public DataTable Parse(string filePath);
+    }
+}
