40ac912e0c3c847e3f07dca0ee702975d317b3d9
AsgerMoellerSpec <asger.moller@specialisterne.com>
2025-10-22 13:13:38 +0200

    Added individual id fields to inventoryitem and orderitem

--- Warehouse Managemet System/RowModels/OrderItem.cs
+++ Warehouse Managemet System/RowModels/OrderItem.cs
@@ -2,6 +2,7 @@ namespace Warehouse_Managemet_System.Table_Models
 {
     class OrderItem : IRowModel
     {
+        public required string Id { get; set; }
         public required string OrderId { get; set; }
         public required string ProductId { get; set; }
         public required int Amount { get; set; }
