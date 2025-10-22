b9221011e8ffee9077935a73c282e588f54f615a
AsgerMoellerSpec <asger.moller@specialisterne.com>
2025-10-22 11:03:52 +0200

    Added Name field to Warehouse class

--- Warehouse Managemet System/RowModels/Warehouse.cs
+++ Warehouse Managemet System/RowModels/Warehouse.cs
@@ -3,5 +3,6 @@ namespace Warehouse_Managemet_System.Table_Models
     class Warehouse : IRowModel
     {
         public required string Id { get; set; }
+        public required string Name { get; set; }
     }
 }
