6ea24602a01b2f86035a49b69b0caed628cda3f9
AsgerMoellerSpec <asger.moller@specialisterne.com>
2025-10-23 12:55:47 +0200

    Added Bogus dependency and implemented GenerateProduct

---
+++ Warehouse Managemet System/DataFaking/DataGenerator.cs
@@ -0,0 +1,51 @@
+using Bogus;
+using Warehouse_Managemet_System.Table_Models;
+
+namespace Warehouse_Managemet_System.DataFaking;
+
+public class DataGenerator : IDataGenerator
+{
+    public Product GenerateProduct(int? seed)
+    {
+        if (seed != null)
+        {
+            Randomizer.Seed = new Random(123);
+        }
+        Faker<Product> productFaker = new Faker<Product>()
+            .RuleFor(r => r.Id, f => "")
+            .RuleFor(r => r.Name, f => f.Commerce.ProductName())
+            .RuleFor(r => r.Price, f => f.Random.Double(0.0, 400.0));
+        Product product = productFaker.Generate();
+        return product;
+    }
+
+    public Warehouse GenerateWarehouse(int? seed)
+    {
+        return null;
+    }
+
+    public InventoryItem GenerateInventoryItem(int? seed)
+    {
+        return null;
+    }
+
+    public Order GenerateOrder(int? seed)
+    {
+        return null;
+    }
+
+    public OrderItem GenerateOrderItem(int? seed)
+    {
+        return null;
+    }
+
+    public Transaction GenerateTransaction(int? seed)
+    {
+        return null;
+    }
+    
+    public List<Product> GenerateRows<RowModel>(int amount, int? seed) where RowModel : IRowModel
+    {
+        return null;
+    }
+}
