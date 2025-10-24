480698e5097cb56674bd5a3668a6ea1461f2fa33
AsgerMoellerSpec <asger.moller@specialisterne.com>
2025-10-24 11:07:32 +0200

    ProductGenerator and WarehouseGenerator now give rows unique ids

--- Warehouse Managemet System/DataFaking/ProductGenerator.cs
+++ Warehouse Managemet System/DataFaking/ProductGenerator.cs
@@ -6,11 +6,12 @@ namespace Warehouse_Managemet_System.DataFaking;
 public class ProductGenerator : IRowGenerator
 {
     private Faker<Product> productFaker;
+    private int nextId = 0;
 
     public ProductGenerator()
     {
         productFaker = new Faker<Product>()
-            .RuleFor(r => r.Id, f => "")
+            .RuleFor(r => r.Id, f => "" + nextId++)
             .RuleFor(r => r.Name, f => f.Commerce.ProductName())
             .RuleFor(r => r.Price, f => f.Random.Double(0.0, 400.0));
     }
--- Warehouse Managemet System/DataFaking/WarehouseGenerator.cs
+++ Warehouse Managemet System/DataFaking/WarehouseGenerator.cs
@@ -6,11 +6,12 @@ namespace Warehouse_Managemet_System.DataFaking;
 public class WarehouseGenerator : IRowGenerator
 {
     private Faker<Warehouse> warehouseFaker;
+    private int nextId = 0;
 
     public WarehouseGenerator()
     {
         warehouseFaker = new Faker<Warehouse>()
-            .RuleFor(r => r.Id, f => "")
+            .RuleFor(r => r.Id, f => "" + nextId++)
             .RuleFor(r => r.Name, f => f.Address.City());
     }
 
