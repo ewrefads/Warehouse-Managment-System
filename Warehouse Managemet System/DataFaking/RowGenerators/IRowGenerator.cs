dd3c0cc34b2b19a191634d94d46eaaaafef5e22d
AsgerMoellerSpec <asger.moller@specialisterne.com>
2025-10-31 12:28:39 +0100

    IRowGenerator and implementations now have documentation

--- Warehouse Managemet System/DataFaking/RowGenerators/IRowGenerator.cs
+++ Warehouse Managemet System/DataFaking/RowGenerators/IRowGenerator.cs
@@ -3,7 +3,14 @@ using Warehouse_Managemet_System.RowModels;
 
 namespace Warehouse_Managemet_System.DataFaking;
 
+/// <summary>
+/// Used for generating fake data rows (instances of IRowModel)
+/// </summary>
 public interface IRowGenerator
 {
+    /// <summary>
+    /// Generates a data row
+    /// </summary>
+    /// <returns>IRowModel instance, fake row</returns>
     public IRowModel Generate();
 }
